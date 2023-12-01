96323edcc2dd31d84a1487c3c88430499861dc48
rajebi <rajebi@yahoo.com>
2023-12-01 21:00:24 +0530

    simple mqtt client

---
+++ MQTTClient/inc/MQTTClient.h
@@ -0,0 +1,189 @@
+﻿//=================================================================================================
+// MQTTClient.h : Include file for standard system include files,
+// or project specific include files.
+
+// mqtt agent need to implement the following
+// 1) agent onboarding, agent initialization
+// 2) subscribe to topics, publish message for the topics, 
+// 3) process inbound message, constructing outbound message based on JSON data model.
+// 4) Invoke the corresponding event handler on receving inboud message
+// 5) Provide callbacks for outbound message to the modules to send outbound message
+
+// call flow
+// Initialize ODD agent -> initialize mqtt client, onboarding, message subscritpion, 
+// Inbound message -> process inbound JSON msg -> invoke respective modules to take action ->
+// callback from modules -> construct outbound JSON msg -> publish to MQTT broker 
+
+
+// Topic prefix
+// strncpy(mqtt_test_device_topic_prefix, mqtt_test_realm, MQTT_TOPIC_MAX_SIZE);
+// strncpy(mqtt_test_device_topic_prefix, "/", MQTT_TOPIC_MAX_SIZE);
+
+// other prefixes ...
+// strncpy(mqtt_test_device_topic_prefix, mqtt_test_device_id, MQTT_TOPIC_MAX_SIZE);
+// strncpy(mqtt_test_device_topic_prefix, "/v0/device_monitor_agent/", MQTT_TOPIC_MAX_SIZE);
+
+// strncpy(mqtt_test_incoming_ifc_topic, mqtt_test_device_topic_prefix, MQTT_TOPIC_MAX_SIZE);
+// strncpy(mqtt_test_incoming_ifc_topic, "incomingifc/v0", MQTT_TOPIC_MAX_SIZE);
+
+//=================================================================================================
+#pragma once
+
+#include <iostream>
+#include <string.h>
+#include <windows.h>
+#include <windows.data.json.h> //https://learn.microsoft.com/en-us/uwp/api/windows.data.json.jsonobject?view=winrt-22621
+#include "C:\Program Files\mosquitto\devel\mosquitto.h"
+
+/*
+To add JSON library
+https://www.youtube.com/watch?v=GYauneigGTs
+
+https://github.com/open-source-parsers/jsoncpp
+
+Mosquitto on windows
+https://cedalo.com/blog/how-to-install-mosquitto-mqtt-broker-on-windows/
+
+*/
+
+
+//#define MQTT_BROKER_HOSTNAME "DESKTOP-QM4I8E2"   // this is the Id I got typing in cmd
+#define MQTT_BROKER_HOSTNAME "localhost"   
+#define MQTT_BROKER_PORT      1883    // default port 
+// the broker would start caring about the status after 300 seconds 
+// without communication between the client and the broker.It waits 
+// for the PINGREQ message from the client.If the client is silent, 
+// the broker disconnects it after another 150 seconds.Likewise, 
+// if the broker does not respond to the client with the PINGRESP message,
+// the client shuts down the connection.
+#define MQTT_BROKER_KEEPALIVE     300
+
+#define MQTT_CLIENT_FAILUE  0
+#define MQTT_CLIENT_SUCCESS 1
+#define MQTT_TEST_TYPE_PUB  1
+#define MQTT_TEST_TYPE_SUB  2
+
+
+class MQTTClient {
+private:
+	char Client_Id[20];
+	int Type;  // type is either PUB or SUB
+	struct mosquitto* mosq_test = NULL;
+	char topic[50] = "/MyMqttTest/Topic1/";  // default topic
+	char* msg;
+
+public:
+	MQTTClient(int val, int t) : Type(t){
+		if (Type == MQTT_TEST_TYPE_PUB) {
+			strncpy(Client_Id, "MQTT_TEST_PUB", sizeof(Client_Id));
+		}else {
+			strncpy(Client_Id, "MQTT_TEST_SUB", sizeof(Client_Id));
+		}
+	}
+	
+	int mqtt_client_init();
+
+	int mqtt_client_connect();
+
+	int mqtt_test_my_type() { return Type; }
+
+	int mqtt_client_publish();
+
+	int mqtt_client_subscribe();
+
+	int mqtt_client_process_msg();
+
+	// callbacks for mosq connections
+	void mqtt_test_connect_cb(struct mosquitto* mosq_pub, void* userdata, int result);
+	void mqtt_test_disconnect_cb(struct mosquitto* mosq_pub, void* userdata, int result);
+	void mqtt_test_publish_cb(struct mosquitto* mosq_pub, void* userdata, int msg_Id);
+	void mqtt_test_subscribe_cb(struct mosquitto* mosq_pub,void* userdata, 
+								int result,int qos_level,const int* given_qos);
+	void mqtt_test_msg_cb(struct mosquitto* mosq_pub, void* userdata, 
+							const struct mosquitto_message* msg);
+	
+	// desctructor
+	virtual ~MQTTClient() {};
+};
+
+
+/*
+JSON schema for messages sent for topics
+
+Shema format - 1
+----------------
+	{
+		"name": "tempereture",
+		"description": "device tempereture"
+		"type":"float"
+	}
+
+Shema format - 2
+----------------
+	{
+		"data1":[{
+					"name": "tempereture",
+					"description": "device tempereture",
+					"type":"float"
+		},{
+					"name": "speed",
+					"description": "device movement",
+					"type":"float"
+		}]
+	}
+
+The actual data from device will be sent as below for the above schema,
+	{
+		"data1":{
+				"tempereture": 25.2,
+				"speed":"60.5
+		}
+	}
+
+This will be translated to REST APIs for topics as below
+	deviceId/data1/tempereture
+	deviceId/data1/speed
+
+
+MQTT can also be used fot implementing basic START, STOP, GET and CONFIG APIs. In this case
+the controller application (mqtt agenet) would publish these API messages and the device agents
+would substribe to them. 
+
+Similarly, the reponse messages for these APIs can also be modelled as PUB/SUB. where the device
+agents would publish those reponse messages and the controller application would substribe to them.
+
+e.g of GET API
+
+	{
+		"device_service":{
+					"name" : "monitoring service",
+					"agent": "temp sensor agent"
+		},
+		"temp_service_request":{
+			"req_type":"GET",
+			"req_id":1,
+			"category":"module1",
+			"data_list":[1,5,10],
+		}
+	}
+
+e.g of START API
+
+	{
+		"device_service":{
+					"name" : "monitoring service",
+					"agent": "temp sensor agent"
+		},
+		"temp_service_request":{
+			"req_type":"GET",
+			"req_id":1,
+			"category":"module1",
+			"data_list":[1,5,10],
+			"duration":600,      # send it for 600 seconds
+			"period":60          # send it every 60s 
+		}
+	}
+				
+
+
+*/
---
+++ MQTTClient/src/MQTTClient.cpp
@@ -0,0 +1,217 @@
+﻿// MQTTClient.cpp : Defines the entry point for the application.
+//
+
+
+#include "MQTTClient.h"
+using namespace std;
+
+
+
+static MQTTClient* mqttClient;  // client instance
+
+//=================== wrapper functions over MQTTClient class =============
+
+void mqtt_test_connect_cb_wrapper(struct mosquitto* mosq_pub,
+    void* userdata,
+    int result) {
+
+    mqttClient->mqtt_test_connect_cb(mosq_pub, userdata, result);
+}
+
+void mqtt_test_disconnect_cb_wrapper(struct mosquitto* mosq_pub,
+    void* userdata,
+    int result) {
+
+    mqttClient->mqtt_test_disconnect_cb(mosq_pub, userdata, result);
+}
+
+void mqtt_test_publish_cb_wrapper(struct mosquitto* mosq_pub,
+    void* userdata,
+    int msg_id) {
+
+    mqttClient->mqtt_test_publish_cb(mosq_pub, userdata, msg_id);
+}
+
+void mqtt_test_subscribe_cb_wrapper(struct mosquitto* mosq_pub, 
+        void* userdata, int msg_id, int qos_count, const int* qos_granted){
+
+    mqttClient->mqtt_test_subscribe_cb(mosq_pub, userdata, msg_id, qos_count, qos_granted);
+}
+
+void mqtt_test_msg_cb_wrapper(struct mosquitto* mosq_pub,
+    void* userdata,const struct mosquitto_message* msg) {
+
+    mqttClient->mqtt_test_msg_cb(mosq_pub, userdata, msg);
+}
+
+
+//================= MQTTClient class member definitions ========================
+void MQTTClient::mqtt_test_connect_cb(
+    struct mosquitto* mosq_pub,
+    void* userdata,
+    int result) {
+
+    cout << "mqtt_test_connect_cb is invoked" << endl;
+
+    // check if this instance is a publisher or subscriber
+    if (Type == MQTT_TEST_TYPE_PUB) {
+        cout << "This is PUB client" << endl;
+        mqtt_client_publish(); 
+
+    }else{
+        cout << "This is SUB client" << endl;
+        mqtt_client_subscribe();
+    }
+
+    return;
+
+}
+
+void MQTTClient:: mqtt_test_disconnect_cb(
+    struct mosquitto* mosq_pub, 
+    void* userdata, 
+    int result) {
+    
+    cout << "mqtt_test_disconnect_cb is invoked" << endl;
+
+}
+
+void MQTTClient:: mqtt_test_publish_cb(
+    struct mosquitto* mosq_pub, 
+    void* userdata, 
+    int msg_Id){
+    
+    cout << "mqtt_test_publish_cb is invoked " << endl;
+
+    Sleep(1000); // publish msg every 10s
+    mqtt_client_publish(); 
+
+}
+void MQTTClient:: mqtt_test_subscribe_cb(
+    struct mosquitto* mosq_pub, 
+    void* userdata, int result,
+    int qos_level, const int *given_qos) {
+    
+    cout << "mqtt_test_subscribe_cb is invoked " << endl;
+}
+
+
+// this method is called when the substriber receives a new message
+void MQTTClient:: mqtt_test_msg_cb(
+    struct mosquitto* mosq_pub, 
+    void* userdata, const struct mosquitto_message* msg){
+    
+    cout << "mqtt_test_msg_cb is invoked : msg Id = " << msg->topic << endl;
+
+    if (!msg->topic || !msg->payload) {
+        cout << "invalid msg Id or msg payload received, discarding the msg" << endl;
+    }
+
+    // check the msg
+    if (strncmp(msg->topic, topic, strnlen(topic, 100)) == 0) {
+        cout << "MQTT msg received: " << (char *) msg->payload << endl;
+        //printf("msg = %s \n", msg->payload);
+    }
+}
+
+int MQTTClient::mqtt_client_publish(){
+
+    char mqtt_msg[50] = "hello";
+    
+    int r = mosquitto_publish(mosq_test, NULL, topic, (strlen(mqtt_msg) + 1), mqtt_msg, 1, false);
+    
+    if (r != MOSQ_ERR_SUCCESS) {
+        cout << "publish failed , err reason = " << r << endl;
+    }
+    else {
+        cout << "publish success , code = " << r << endl;
+    }
+
+    return MQTT_CLIENT_SUCCESS;
+}
+
+int MQTTClient::mqtt_client_subscribe() {
+
+    // If this instance is a subscriber, subscribe incoming message from 
+    int r = mosquitto_subscribe(mosq_test, NULL, topic, 1);
+
+    if (r != MOSQ_ERR_SUCCESS) {
+        cout << "subscribe failed , err reason = " << r << endl;
+    }
+    else {
+        cout << "subscribe success , code = " << r << endl;
+    }
+    return MQTT_CLIENT_SUCCESS;
+}
+
+
+int MQTTClient::mqtt_client_connect() {
+    
+    int res = mosquitto_connect(mosq_test, "127.0.0.1", 1883, MQTT_BROKER_KEEPALIVE);
+    if (res == MOSQ_ERR_SUCCESS) {
+        cout << "connect success : start the loop " << res << endl;
+        // start the mosquitto loop ; refer https://mosquitto.org/api/files/mosquitto-h.html
+        // this is part of the threaded client interface.
+        //res = mosquitto_loop_start(mosq_test);  // this is a non-blocking call
+        res = mosquitto_loop_forever(mosq_test, 1000, 100); // this is a blocking call.
+        cout << "loop start result " << res << endl;
+
+    }
+    else { cout << "connect failed: " << res << endl; }
+
+    return res;
+}
+
+int MQTTClient::mqtt_client_init() {
+    int res;
+
+    cout << "mqtt client is initialized" << endl;
+
+    // call the mosquito library init
+    res = mosquitto_lib_init();
+
+    // get a context for mosquitto broker
+    mosq_test = mosquitto_new(Client_Id, true, NULL);
+
+    if (!mosq_test) {
+        cout << "mosq new command failed" << endl;
+        return MQTT_CLIENT_FAILUE;
+    }
+
+    // register callbacks and connect to the broker
+    mosquitto_connect_callback_set(mosq_test, mqtt_test_connect_cb_wrapper);
+    mosquitto_disconnect_callback_set(mosq_test, mqtt_test_disconnect_cb_wrapper);
+    mosquitto_publish_callback_set(mosq_test, mqtt_test_publish_cb_wrapper);
+    mosquitto_subscribe_callback_set(mosq_test, mqtt_test_subscribe_cb_wrapper);
+    mosquitto_message_callback_set(mosq_test, mqtt_test_msg_cb_wrapper);
+    
+    cout << "call backs registered" << endl;
+    
+    res = mqtt_client_connect();
+
+    return MQTT_CLIENT_SUCCESS;
+}
+
+
+void mqtt_create_client(char *type){
+
+    if (strcmp(type, "SUB") == 0) {
+        mqttClient = new MQTTClient(1, MQTT_TEST_TYPE_SUB);
+    }
+    else {
+        mqttClient = new MQTTClient(1, MQTT_TEST_TYPE_PUB);
+    }
+    mqttClient->mqtt_client_init();
+}
+
+void mqtt_delete_client() {
+    // this is the instance of MQTT client
+    delete mqttClient;
+}
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello MQTT client : " << argv[1] << endl;
+    mqtt_create_client(argv[1]);
+    return 0;
+}
